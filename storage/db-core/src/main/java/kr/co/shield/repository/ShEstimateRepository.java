ad3262fcc3d270fa95c73ec47640f61fe84a47a1
asj2 <asj@bizspring.co.kr>
2024-02-03 19:43:22 +0900

    fix: del legacy

--- storage/db-core/src/main/java/kr/co/shield/repository/ShEstimateRepository.java
+++
@@ -1,9 +0,0 @@
-package kr.co.shield.repository;
-
-import kr.co.shield.domain.Estimate;
-import kr.co.shield.domain.LegacyEstimate;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface ShEstimateRepository extends JpaRepository<LegacyEstimate, Integer>, JpaSpecificationExecutor<LegacyEstimate> {
-}
